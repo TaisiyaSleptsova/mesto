35280c88b89677595a5a67955db70e61033e3311
Taisiya Sleptsova <tasyushka@mail.ru>
2023-03-12 15:47:08 +0300

    Сверстать проект место

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const popupElement = document.querySelector(".popup");
+const popupCloseButtomElement = popupElement.querySelector(".popup__close");
+const popupOpenButtomElement = document.querySelector(".profile__edit-button");
+const profileNameElement = document.querySelector(".profile__title");
+const profileJobElement = document.querySelector(".profile__subtitle");
+const formElement = popupElement.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__form_input-name");
+const jobInput = formElement.querySelector(".popup__form_input-job");
+const likeButtomElement = document.querySelector(".element__like");
+
+const openPopup = function () {
+    popupElement.classList.add('popup__opened');
+    nameInput.value = profileNameElement.textContent;
+    jobInput.value = profileJobElement.textContent;
+}
+
+const closePopup = function () {
+  popupElement.classList.remove('popup__opened');
+  };
+
+const handleFormSubmit = function (evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = nameInput.value;
+  profileJobElement.textContent = jobInput.value;
+  closePopup ();
+};
+
+const putLike = function () {
+  likeButtomElement.classList.toggle('element__like_active');
+}
+
+popupOpenButtomElement.addEventListener("click", openPopup);
+popupCloseButtomElement.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleFormSubmit);
+likeButtomElement.addEventListener("click", putLike);
